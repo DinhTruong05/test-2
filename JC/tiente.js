401274c5dd782a1614c3ee84c8bca77b06867721
DinhTruong05 <dinhtruong051095@gmail.com>
2025-05-12 14:00:26 +0700

    luu file jc

---
+++ JC/tiente.js
@@ -0,0 +1,21 @@
+// document.getElementById('convert').addEventListener('click', function () {
+//     const amount = parseFloat(document.getElementById('amount').value);
+//     const from = document.getElementById('from currency').value;
+//     const to = document.getElementById('to currency').value;
+//
+//     let rate = 1;
+//
+//     // Tỷ giá ví dụ
+//     if (from === 'VND' && to === 'USD') {
+//         rate = 0.0000438789;
+//     } else if (from === 'USD' && to === 'VND') {
+//         rate = 22800;
+//     } else {
+//         rate = 1; // cùng loại tiền
+//     }
+//
+//     const result = amount * rate;
+//
+//     // Hiển thị kết quả
+//     document.getElementById('result').innerHTML = `Result: ${result} ${to}`;
+// });

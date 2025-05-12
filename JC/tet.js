38eec9fd3094d3089e49a3bdee01670c3b064973
DinhTruong05 <dinhtruong051095@gmail.com>
2025-05-12 10:49:43 +0700

    luu file jc

--- JC/tet.js
+++ JC/tet.js
@@ -1,4 +1,7 @@
 // inputwight = prompt('Enter your width');
 // inputheight = prompt('Enter your height');
-inputWidth = prompt("Enter Width:");
-inputHeight = prompt("Enter Height:");
+// inputWidth = prompt("Enter Width:");
+// inputHeight = prompt("Enter Height:");
+inputtoan = prompt("Nhập điểm toán");
+inputly = prompt("Nhập điểm lý");
+inputhoa = prompt("Nhập điểm hóa");
